4f3ab8b6d4b84c59a04f4b430604a78c468313f0
DJ Nrrd <djnrrd@gmail.com>
2021-05-22 17:26:15 +0100

    Added random function

--- sd_audio.py
+++ sd_audio.py
@@ -2,6 +2,7 @@ import argparse
 from pydub import AudioSegment
 from pydub.playback import play
 import os
+import random
 
 
 def add_args():
@@ -10,11 +11,22 @@ def add_args():
                                          'fanceee', 'victory',
                                          'glitter_attack', 'horny_jail',
                                          'i_alone', 'princess', 'taps',
-                                         'that_doesnt', 'yikes_forever'])
+                                         'that_doesnt', 'yikes_forever',
+                                         'wrestling'])
     return parser
 
 
+def get_random_file(file_path):
+    base_path = os.path.dirname(__file__)
+    print(base_path)
+    dir_files = os.listdir(f"{base_path}/res/{file_path}")
+    ret_file = random.choice(dir_files)[:-4]
+    return f"{file_path}/{ret_file}"
+
+
 def play_file(file_name):
+    if file_name == 'wrestling':
+        file_name = get_random_file(file_name)
     base_path = os.path.dirname(__file__)
     mp3_path = f"{base_path}/res/{file_name}.mp3"
     file_sound = AudioSegment.from_mp3(mp3_path)
